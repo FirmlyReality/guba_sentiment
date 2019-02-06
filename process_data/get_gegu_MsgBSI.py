35635bb34ec269e6979a1d2b941460af60b3dede
Firmlyzhu <firmlyzhu@163.com>
2019-02-06 21:11:36 +0800

    update

--- process_data/get_gegu_MsgBSI.py
+++ process_data/get_gegu_MsgBSI.py
@@ -3,7 +3,7 @@ import numpy as np
 import tushare as ts
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-import math, sys
+import math, sys, os
 import statsmodels.tsa.stattools as tsa
 from datetime import datetime, timedelta
 
@@ -45,78 +45,98 @@ if __name__ == "__main__":
     outputDir = sys.argv[2]
 
     stocks300 = ts.get_k_data('000300', index=True,start="2015-01-01",end="2018-10-01")
-    stocks_date = ['2015-01-01'] + list(stocks300.date)
-    print(stocks_date)
-    exit(0)
+    #date_300 = ['2015-01-01'] + list(stocks300.date)
+    #print(date_300)
+    #exit(0)
     
     print("Read from inputDir %s" % inputDir)
     files = os.listdir(inputDir)
     files = sorted(files)
     
     for file in files:
-        print("Process file: %s"%file)
+        code = file.split(".")[0]
+        print("Process file: %s, stock_code:%s "%(file, code))
+        
+        stock_data = ts.get_k_data(code=code, start='2015-01-01', end='2018-10-01')
+        #print(stock_data)
+        if len(stock_data) == 0:
+            print("Code %s does not exist!!!" % code)
+            stock_data = stocks300
+        stock_date = ['2015-01-01'] + list(stock_data.date)
+        #print(stock_date)
         #fig = plt.figure()
         #ax1 = fig.add_subplot(111)
         #plt.plot(datesl,indicators_pre,'b',alpha=0.9)
         #plt.plot(datesl,indicators_int,alpha=0.9)
-        sum_data = pd.read_csv(file)
+        sum_data = pd.read_csv(os.path.join(inputDir,file))
         sum_data.index = sum_data['date']
         #print(sum_data)
         
         aftMsgBSI = []
         intMsgBSI = []
-        allMsgBSI = []
-        for i in range(1,len(stocks_date)):
-            now_date = datetime.strptime(stocks_date[i],"%Y-%m-%d")
-            last_date = datetime.strptime(stocks_date[i-1],"%Y-%m-%d")
+        aftallMsgBSI = []
+        preMsgBSI = []
+        preallMsgBSI = []
+        for i in range(1,len(stock_date)):
+            now_date = datetime.strptime(stock_date[i],"%Y-%m-%d")
+            last_date = datetime.strptime(stock_date[i-1],"%Y-%m-%d")
             
-            aft_t_cnt = np.zeros(4)
-            aft_r_cnt = np.zeros(4)
-            aft_t_sum = np.zeros(4)
-            aft_r_sum = np.zeros(4)
-            int_t_cnt = np.zeros(4)
-            int_r_cnt = np.zeros(4)
-            int_t_sum = np.zeros(4)
-            int_r_sum = np.zeros(4)            
-            all_t_cnt = np.zeros(4)
-            all_r_cnt = np.zeros(4)
-            all_t_sum = np.zeros(4)
-            all_r_sum = np.zeros(4)
+            date_str = now_date.strftime("%Y-%m-%d")
+            d = sum_data.loc[date_str]
+            aft_t_cnt = np.array([d['aft_t_'+str(i)+'_cnt'] for i in range(4)])
+            aft_r_cnt = np.array([d['aft_r_'+str(i)+'_cnt'] for i in range(4)])
+            aft_t_sum = np.array([d['aft_t_'+str(i)+'_prob'] for i in range(4)])
+            aft_r_sum = np.array([d['aft_r_'+str(i)+'_prob'] for i in range(4)])
+            
+            int_t_cnt = np.array([d['int_t_'+str(i)+'_cnt'] for i in range(4)])
+            int_r_cnt = np.array([d['int_r_'+str(i)+'_cnt'] for i in range(4)])
+            int_t_sum = np.array([d['int_t_'+str(i)+'_prob'] for i in range(4)])
+            int_r_sum = np.array([d['int_r_'+str(i)+'_prob'] for i in range(4)])
+            
+            aftall_t_cnt = (aft_t_cnt + int_t_cnt)
+            aftall_r_cnt = (aft_r_cnt + int_r_cnt)
+            aftall_t_sum = (aft_t_sum + int_t_sum)
+            aftall_r_sum = (aft_r_sum + int_r_sum)            
+            
+            pre_t_cnt = np.zeros(4)
+            pre_r_cnt = np.zeros(4)
+            pre_t_sum = np.zeros(4)
+            pre_r_sum = np.zeros(4)
             
             date = now_date
-            Beta = 0.8
+            Beta = 0.6
             beta_now = 1
             while date > last_date:
-                d = sum_data.loc[date]
-                #print("Compute PreMsgBSI...")
-                aft_t_cnt += beta_now * np.array([d['aft_t_'+str(i)+'_cnt'] for i in range(4)])
-                aft_r_cnt += beta_now * np.array([d['aft_r_'+str(i)+'_cnt'] for i in range(4)])
-                aft_t_sum += beta_now * np.array([d['aft_t_'+str(i)+'_prob'] for i in range(4)])
-                aft_r_sum += beta_now * np.array([d['aft_r_'+str(i)+'_prob'] for i in range(4)])
-                
-                #print("Compute IntMsgBSI...")
-                int_t_cnt += beta_now * np.array([d['int_t_'+str(i)+'_cnt'] for i in range(4)])
-                int_r_cnt += beta_now * np.array([d['int_r_'+str(i)+'_cnt'] for i in range(4)])
-                int_t_sum += beta_now * np.array([d['int_t_'+str(i)+'_prob'] for i in range(4)])
-                int_r_sum += beta_now * np.array([d['int_r_'+str(i)+'_prob'] for i in range(4)])
-                
-                #print("Compute AllMsgBSI...")
-                all_t_cnt += beta_now * (aft_t_cnt + int_t_cnt)
-                all_r_cnt += beta_now * (aft_r_cnt + int_r_cnt)
-                all_t_sum += beta_now * (aft_t_sum + int_t_sum)
-                all_r_sum += beta_now * (aft_r_sum + int_r_sum)
-                
                 date -= timedelta(days=1)
+                date_str = date.strftime("%Y-%m-%d")
+                d = sum_data.loc[date_str]
+                pre_t_cnt += beta_now * np.array([d['aft_t_'+str(i)+'_cnt'] for i in range(4)])
+                pre_r_cnt += beta_now * np.array([d['aft_r_'+str(i)+'_cnt'] for i in range(4)])
+                pre_t_sum += beta_now * np.array([d['aft_t_'+str(i)+'_prob'] for i in range(4)])
+                pre_r_sum += beta_now * np.array([d['aft_r_'+str(i)+'_prob'] for i in range(4)])
+                
+                if date == last_date:
+                    preMsgBSI.append(compute_MsgBSI(pre_t_cnt, pre_r_cnt, pre_t_sum, pre_r_sum))
+                pre_t_cnt += beta_now * np.array([d['int_t_'+str(i)+'_cnt'] for i in range(4)])
+                pre_r_cnt += beta_now * np.array([d['int_r_'+str(i)+'_cnt'] for i in range(4)])
+                pre_t_sum += beta_now * np.array([d['int_t_'+str(i)+'_prob'] for i in range(4)])
+                pre_r_sum += beta_now * np.array([d['int_r_'+str(i)+'_prob'] for i in range(4)])
+                           
                 beta_now *= Beta
                 
             aftMsgBSI.append(compute_MsgBSI(aft_t_cnt, aft_r_cnt, aft_t_sum, aft_r_sum))
             intMsgBSI.append(compute_MsgBSI(int_t_cnt, int_r_cnt, int_t_sum, int_r_sum))
-            allMsgBSI.append(compute_MsgBSI(all_t_cnt, all_r_cnt, all_t_sum, all_r_sum))
-        preMsgBSI = aftMsgBSI[:-1]
-
-        stock_data = ts.get_k_data(code=code, start='2015-01-01', end='2018-10-01')
-        print(stock_data)
-        stock_date = list(stock_data.date)
+            aftallMsgBSI.append(compute_MsgBSI(aftall_t_cnt, aftall_r_cnt, aftall_t_sum, aftall_r_sum))          
+            preallMsgBSI.append(compute_MsgBSI(pre_t_cnt, pre_r_cnt, pre_t_sum, pre_r_sum))
+            
+        MsgBSI_data = pd.DataFrame()
+        MsgBSI_data['date'] = stock_date[1:]
+        MsgBSI_data['preMsgBSI'] = preMsgBSI
+        MsgBSI_data['intMsgBSI'] = intMsgBSI
+        MsgBSI_data['aftMsgBSI'] = aftMsgBSI
+        MsgBSI_data['preallMsgBSI'] = preallMsgBSI
+        MsgBSI_data['aftallMsgBSI'] = aftallMsgBSI
+        MsgBSI_data.to_csv(os.path.join(outputDir,file),index=False)
         
         stock_open = list(stock_data.open)
         stock_close = list(stock_data.close)
@@ -147,39 +167,44 @@ if __name__ == "__main__":
         
         print("ADF test for intMsgBSI:")
         print(tsa.adfuller(intMsgBSI))
-        
-        print("ADF test for allMsgBSI:")
-        print(tsa.adfuller(allMsgBSI))
-        
+
         print("ADF test for aftMsgBSI:")
         print(tsa.adfuller(aftMsgBSI))
-                  
+        
+        print("ADF test for preallMsgBSI:")
+        print(tsa.adfuller(preallMsgBSI))
+        
+        print("ADF test for aftallMsgBSI:")
+        print(tsa.adfuller(aftallMsgBSI))
+               
         #stock_returns = np.array(stock_returns)*100
-        #plt.plot(stock_date,stock_returns,'r',alpha=0.9)
+        
+        print(len(preMsgBSI))
+        print(len(today_returns))
 
         print("Fit today_returns-preMsgBSI:")
-        fit_linear(today_returns[1:],preMsgBSI)
+        fit_linear(today_returns,preMsgBSI)
         
         print("Fit today_returns-intMsgBSI:")
         fit_linear(today_returns,intMsgBSI)
         
-        print("Fit today_returns-allMsgBSI[-1]:")
-        fit_linear(today_returns[1:],allMsgBSI[:-1])
+        #print("Fit today_returns-allMsgBSI[-1]:")
+        #fit_linear(today_returns[1:],allMsgBSI[:-1])
 
         print("Fit today_returns-aftMsgBSI:")
         fit_linear(today_returns,aftMsgBSI)
         
         print("Fit open_returns-preMsgBSI:")
-        fit_linear(open_returns,preMsgBSI)       
+        fit_linear(open_returns,preMsgBSI[1:])       
         
-        print("Fit open_returns-allMsgBSI[-1]:")
-        fit_linear(open_returns,allMsgBSI[:-1])
+        print("Fit open_returns-intMsgBSI[-1]:")
+        fit_linear(open_returns,intMsgBSI[:-1])
         
-        print("Fit close_returns-allMsgBSI:")
-        fit_linear(close_returns,allMsgBSI[:-1])
+        print("Fit close_returns-preallMsgBSI:")
+        fit_linear(close_returns,preallMsgBSI[1:])
         
         print("Fit close_returns-preMsgBSI:")
-        fit_linear(close_returns,preMsgBSI)
+        fit_linear(close_returns,preMsgBSI[1:])
         
         print("Fit close_returns-intMsgBSI[-1]:")
         fit_linear(close_returns,intMsgBSI[:-1])
